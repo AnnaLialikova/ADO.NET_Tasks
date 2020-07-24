10477aeaa310b873d798c0b2f153d7eec2181f50
Ляликова Анна <65788392+AnnaLialikova@users.noreply.github.com>
2020-07-24 16:44:37 +0300

    Зачет

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            App.f1 = this;
+            InitializeComponent();
+            var time = DateTime.Today;
+            string a = time.ToString("yyyy-mm-dd");
+            
+            TextBox1.Text = a.ToString();
+            string connectionString = ("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=NotesDB;Integrated Security=True");
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            StringBuilder results = new StringBuilder();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                string commandText2 = string.Format("SELECT CUSTOMERID, TextReminder, DateCreated FROM DETAILS.REMINDERS WHERE DateCreated = GETDATE()") ;
+                    SqlCommand command2 = new SqlCommand(commandText2, connection);
+                    SqlDataReader reader = command2.ExecuteReader();
+                while (reader.Read())
+                {
+                   for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        results.Append(reader[i].ToString() + "\t");
+                    }
+                    results.Append(Environment.NewLine);
+
+                }
+                outTextBox.Text = results.ToString();
+                reader.Close();
+                    connection.Close();
+                }
+            
+
+        }
+
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            NewTaskWindow NW = new NewTaskWindow();
+            NW.Show();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Tasks_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Authorization_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void getPremium_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FAQ_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Archive_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Notifications_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Notes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            App.f1.outTextBox.Text = null;
+            string connectionString = ("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=NotesDB;Integrated Security=True");
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            StringBuilder results = new StringBuilder();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                DateTime time = datePicker.SelectedDate.Value;
+                string commandText2 = string.Format("SELECT CUSTOMERID, TextReminder, DateCreated FROM DETAILS.REMINDERS WHERE DateCreated = '" + time.ToString("yyyy-MM-dd") + "'");
+                SqlCommand command2 = new SqlCommand(commandText2, connection);
+                SqlDataReader reader = command2.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        results.Append(reader[i].ToString() + "\t");
+                    }
+                    results.Append(Environment.NewLine);
+
+                }
+                outTextBox.Text = results.ToString();
+                reader.Close();
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/NewTaskWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    public partial class NewTaskWindow : Window
+    {
+        public NewTaskWindow()
+        {
+            InitializeComponent();
+            date1.Text = Convert.ToString(DateTime.Now);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            int id_d = getID();
+            string connectionString = string.Format("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=NotesDB;Integrated Security=True");
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                DateTime time = datePicker.SelectedDate.Value;
+                string commandText1 = string.Format("INSERT INTO  DETAILS.REMINDERS (CUSTOMERID, ReminderID, Theme, TextReminder, DateCreated, DateToDo) VALUES ( @CUSTOMERID, @ReminderID,@Theme,@TextReminder,@DateCreated,@DateToDo)") ;
+                SqlCommand command1 = new SqlCommand(commandText1, connection);
+                command1.Parameters.AddWithValue("@CUSTOMERID", 1);
+                command1.Parameters.AddWithValue("@ReminderID", id_d);
+                command1.Parameters.AddWithValue("@Theme", themeBox.Text);
+                command1.Parameters.AddWithValue("@TextReminder", newTaskBox.Text);
+                command1.Parameters.AddWithValue("@DateCreated", date1.Text); 
+                command1.Parameters.AddWithValue("@DateToDo", time.ToString("yyyy-MM-dd"));
+
+                command1.ExecuteNonQuery();
+                    connection.Close();
+                }
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                StringBuilder results = new StringBuilder();
+                DateTime time = datePicker.SelectedDate.Value;
+                string commandText2 = string.Format("SELECT ReminderID, Theme, TextReminder, DateCreated, DateToDo FROM DETAILS.REMINDERS WHERE Date = ('{0}') ORDER BY DateCreated DESC", datePicker);
+                SqlCommand command2 = new SqlCommand(commandText2, connection);
+                SqlDataReader reader = command2.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        results.Append(reader[i].ToString() + "\t");
+                    }
+                    results.Append(Environment.NewLine);
+
+                }
+                newTaskBox.Text = results.ToString();
+                reader.Close();
+                connection.Close();
+            }
+            this.Close();
+        }
+
+        private int getID()
+        {
+            string connectionString = string.Format("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=NotesDB;Integrated Security=True");
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command1 = new SqlCommand("SELECT MAX(ReminderID) FROM DETAILS.REMINDERS", connection);
+                int id = Convert.ToInt32(command1.ExecuteScalar()) + 1;
+                connection.Close();
+                return id;
+            }
+            
+        }
+        private void timeBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void newTaskBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
